3eb90c010b8ffbcd962b17d16ae672bbd2be13fc
Zach Mead <zachary.n.mead@gmail.com>
2021-02-21 22:18:54 -0600

    rendering CRA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import rp from "request-promise";
+import cheerio from "cheerio";
 import './App.css';
 
 function App() {
+  let [names, setNames] = useState([]);
+
+  useEffect(() => {
+    rp("https://pokedex.org/")
+      .then(html => {
+        let names = [];
+        let $ = cheerio.load(html);
+
+        $("#players-list li span").each(function (i, element) {
+          let name = $(this)
+            .prepend()
+            .text();
+          names.push(name);
+
+        });
+
+        setNames([names]);
+      })
+      .catch(function(err) {
+        console.log("crawl failed");
+      });
+  }, []);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +47,13 @@ function App() {
           Learn React
         </a>
       </header>
-
+      <div>
+        <ul>
+          {names.map((name) => {
+            return <li key={names.name}>{names.name}</li>;
+          })}
+        </ul>
+      </div>
 
     </div>
   );
