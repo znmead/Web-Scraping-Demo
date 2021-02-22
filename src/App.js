9048321f8ad2486d29eed0d31b1444c87607d893
Zach Mead <zachary.n.mead@gmail.com>
2021-02-21 20:43:52 -0600

    added useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import rp from "request-promise";
 import './App.css';
 
